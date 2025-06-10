23f6a048fbc8d8783d64ac33fbd0f40f40c0d9f3
MarianoT1 <marianotoledo311@gmail.com>
2025-06-10 17:08:52 -0300

    Listo los puntos del 1 al 5, ahora en un rato hago del 6 al 8.

---
+++ src/Funciones.java
@@ -0,0 +1,59 @@
+
+
+public class Funciones {
+    //Clase que voy a utilizar para Funciones Basicas
+    public static void main(String[] args) {
+
+        Saludo();
+        System.out.println("");
+        String Nombre = "Mariano";
+        saludar(Nombre);
+        System.out.println("");
+
+        int base = 2;
+        int altura = 7;
+        int area= calculararea(base, altura);
+        System.out.printf("La base del rectangulo es de: " + area);
+        System.out.println("");
+
+        String mensaje = "Ma levanta la ropa que está por llover";
+        Mayuscula(mensaje);
+        System.out.println(Mayuscula(mensaje));
+        Menu();
+    }
+
+    //Metodo de saludo simple
+    public static void Saludo(){
+        System.out.println(" Sean Bienvenidos a las Funciones de Java ");
+    }
+
+    //Metodo para saludar con el Nombre
+    public static void saludar(String Nombre){
+        System.out.printf(" Hola mundo, soy " + Nombre);
+    }
+
+    //Metodo para calcular el area de un rectangulo
+    public static int calculararea (int base, int altura){
+        int area = base * altura;
+        return  area;
+    }
+
+    //Metodo para las Mayusculas
+    public static String Mayuscula (String mensaje){
+        return mensaje.toUpperCase();
+    }
+
+    //Metodo para el menú (menu de opcines, no el menú de comer)
+    public static void Menu(){
+        System.out.println("  Opciones  ");
+        System.out.println("1. Cocinar");
+        System.out.println("2. Encargar");
+        System.out.println("3. N0 Comer");
+    }
+
+}
+
+
+
+
+
